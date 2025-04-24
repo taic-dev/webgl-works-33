9591b78f73fba530499e73d9c4f275c62f38dd38
taic-dev <c.t.o.taishi.0530@gmail.com>
2025-04-24 22:06:16 +0900

    fix

--- src/assets/ts/app.ts
+++ src/assets/ts/app.ts
@@ -5,28 +5,30 @@ import { opening } from "./modules/opening";
 import { Scroll } from "./modules/Scroll";
 import { Mouse } from "./modules/Mouse";
 
-// webgl
-const webgl = new App();
-webgl.init();
-gsap.ticker.add(() => {
-  webgl.render();
-  webgl.update();
-});
-
-// animation
-opening();
-const animation = new Animation(webgl.setup, webgl.mesh);
-animation.openingGl();
-
-// scroll
-const scroll = new Scroll();
-scroll.init();
-
-// mouse
-const mouse = new Mouse(webgl.mesh);
-mouse.init();
-
-window.addEventListener("resize", () => {
-  webgl.resize();
-  scroll.resize();
-});
+window.addEventListener('load', () => {
+  // webgl
+  const webgl = new App();
+  webgl.init();
+  gsap.ticker.add(() => {
+    webgl.render();
+    webgl.update();
+  });
+  
+  // animation
+  opening();
+  const animation = new Animation(webgl.setup, webgl.mesh);
+  animation.openingGl();
+  
+  // scroll
+  const scroll = new Scroll();
+  scroll.init();
+  
+  // mouse
+  const mouse = new Mouse(webgl.mesh);
+  mouse.init();
+  
+  window.addEventListener("resize", () => {
+    webgl.resize();
+    scroll.resize();
+  });
+})
